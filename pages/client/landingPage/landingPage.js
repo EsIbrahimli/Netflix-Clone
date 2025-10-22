16fb775fabb2566dd81b30f3268eace8893bd097
Asmar Ibrahimli <ibrahimli.asmar@gmail.com>
2025-10-23 03:10:27 +0400

    detailed figma modified

---
+++ pages/client/landingPage/landingPage.js
@@ -0,0 +1,9 @@
+
+const questions = document.querySelectorAll(".faq-question");
+    questions.forEach(q => {
+      q.addEventListener("click", () => {
+        q.classList.toggle("active");
+        const answer = q.nextElementSibling;
+        answer.style.display = answer.style.display === "block" ? "none" : "block";
+      });
+    });
