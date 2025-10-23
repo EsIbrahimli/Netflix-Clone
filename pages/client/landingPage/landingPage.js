a2f2567ce7f61738f74a9e6f922a9df3041eb1ac
Asmar Ibrahimli <ibrahimli.asmar@gmail.com>
2025-10-23 15:07:51 +0400

    landingPage deleted temprorely

--- pages/client/landingPage/landingPage.js
+++
@@ -1,9 +0,0 @@
-
-const questions = document.querySelectorAll(".faq-question");
-    questions.forEach(q => {
-      q.addEventListener("click", () => {
-        q.classList.toggle("active");
-        const answer = q.nextElementSibling;
-        answer.style.display = answer.style.display === "block" ? "none" : "block";
-      });
-    });
