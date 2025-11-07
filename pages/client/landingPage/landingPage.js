bd176f35c1b84957c0d6470a6f88fee3c97974e6
Asmar Ibrahimli <ibrahimli.asmar@gmail.com>
2025-11-07 18:06:34 +0400

    Update asset paths in landingPage.js for consistent navigation to login, logout, and registration pages

--- pages/client/landingPage/landingPage.js
+++ pages/client/landingPage/landingPage.js
@@ -112,7 +112,7 @@ if (token) {
     });
 } else {
     signInBtn.addEventListener('click', () => {
-        window.location.href = '../login/login.html';
+        window.location.href = '/pages/client/login/login.html';
     });
 }
 
@@ -122,7 +122,7 @@ if (logoutBtn) {
         e.preventDefault();
         localStorage.removeItem('token');
         localStorage.removeItem('user');
-        window.location.href = '../../../index.html';
+        window.location.href = '/index.html';
     });
 }
 
@@ -140,7 +140,7 @@ filmalisaForm.addEventListener('submit', (e) => {
     const email = emailInput.value;
     if(!token && email){
     localStorage.setItem('pendingEmail', email);
-    window.location.href = "../../../pages/client/registr/registr.html";
+    window.location.href = "/pages/client/registr/registr.html";
     } else if(!token && !email){
         emailInput.style.border = '1px solid red';
         setTimeout(() => {
