7cb5c1506c1eb04ad0f8a65aba7f679a106ac484
Asmar Ibrahimli <ibrahimli.asmar@gmail.com>
2025-11-07 17:55:18 +0400

    Update asset path in landingPage.js for correct navigation to login page

--- pages/client/landingPage/landingPage.js
+++ pages/client/landingPage/landingPage.js
@@ -112,7 +112,7 @@ if (token) {
     });
 } else {
     signInBtn.addEventListener('click', () => {
-        window.location.href = '../../../pages/client/login/login.html';
+        window.location.href = './../login/login.html';
     });
 }
 
